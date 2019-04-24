97fadba4b09e889cedd4da00396fe5504e61a5a7
Ruamemanuel <49948798+Ruamemanuel@users.noreply.github.com>
2019-04-24 15:27:59 -0300

    Lista_3

---
+++ Lista_3.R
@@ -0,0 +1,30 @@
+##Questão 2####
+x <- 38
+
+y <- 42
+
+z <- (x+y)
+z
+
+cpf <- 11300273410
+
+z * cpf
+
+##Questão 3####
+
+head(mtcars) #importando os dados
+
+dim(mtcars) #número de dimensão da base
+
+str (mtcars) #descrição dos tipos de variáveis
+
+mtcars [ ,3] #impressão da terceira coluna
+
+mtcars [2, ] #impressão da segunda linha
+
+mtcars$cyl[4] #4º elemento na variável cyl
+
+summary(mtcars) # resumo das variáveis 
+
+##Questão 4####
+
